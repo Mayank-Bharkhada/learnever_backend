1dbb340a0346b2fd8a082f2790f80c0cc19b7f56
Dharmik Valani <dharmikvalani57@gmail.com>
2025-06-06 00:11:25 +0530

    Remove Dist Folder

--- dist/src/routes/auth.router.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import { signIn, signUp, verifyToken } from '../controllers/auth.controller.js';
-import { authenticateUser } from '../middlewares/auth.js';
-export var authRouter = Router();
-authRouter.post('/signUp', signUp);
-authRouter.post('/signIn', signIn);
-authRouter.post('/verifyToken', authenticateUser, verifyToken);
